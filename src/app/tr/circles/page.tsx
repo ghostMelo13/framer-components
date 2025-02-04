b7750113da9a04d2a0dba5fba53718f3b91f4435
Amar Prasad <amarprasad77354@gmail.com>
2025-02-04 22:02:29 +0530

    changes in loc

--- src/app/tr/circles/page.tsx
+++ src/app/tr/circles/page.tsx
@@ -3,7 +3,7 @@
 export default function AnimatedHalfCircles() {
   return (
     <div className="w-full min-h-screen flex items-center justify-center bg-gray-900 py-9">
-      <svg className="absolute -top-96" width="1067" height="1050" viewBox="0 0 1067 1050" fill="none" xmlns="http://www.w3.org/2000/svg">
+       <svg width="1067" height="1050" viewBox="0 0 1067 1050" fill="none" xmlns="http://www.w3.org/2000/svg">
         <circle cx="533.5" cy="516.5" r="487" stroke="url(#paint0_linear_133_105)" strokeDasharray="12 12" />
         <circle cx="533.5" cy="508.5" r="451" stroke="url(#paint1_linear_133_105)" strokeDasharray="12 12" />
         <circle cx="534" cy="516" r="405.5" stroke="url(#paint2_linear_133_105)" strokeDasharray="12 12" />
@@ -11,13 +11,21 @@ export default function AnimatedHalfCircles() {
 
         {/* Animated circles */}
         <circle r="8" fill="#A972D9" opacity="0.8">
-          <animateMotion dur="10s" repeatCount="indefinite" path="M1020.5,516.5 A487,487 0 1,1 46.5,516.5" />
+          <animateMotion
+            dur="10s"
+            repeatCount="indefinite"
+            path="M46.5,516.5 A487,487 0 0,0 1020.5,516.5 A487,487 0 0,0 46.5,516.5"
+          />
         </circle>
         <circle r="8" fill="#A972D9" opacity="0.8">
           <animateMotion dur="9s" repeatCount="indefinite" path="M984.5,508.5 A451,451 0 1,1 82.5,508.5" />
         </circle>
         <circle r="8" fill="#A972D9" opacity="0.8">
-          <animateMotion dur="8s" repeatCount="indefinite" path="M939.5,516 A405.5,405.5 0 1,1 128.5,516" />
+          <animateMotion
+            dur="8s"
+            repeatCount="indefinite"
+            path="M128.5,516 A405.5,405.5 0 0,0 939.5,516 A405.5,405.5 0 0,0 128.5,516"
+          />
         </circle>
         <circle r="8" fill="#A972D9" opacity="0.8">
           <animateMotion dur="11s" repeatCount="indefinite" path="M1066.5,516.5 A533,533 0 1,1 0.5,516.5" />
